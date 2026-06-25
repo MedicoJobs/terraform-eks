7add2db1cab7eb4106b33f6c9df1d0dbed9f97e5
PaviTechDevloper <pavikutty0609@gmail.com>
2026-06-25 16:10:38 +0530

    Use S3 Remote Backend for state management

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket         = "medicojobs-terraform-state-168614391879"
+    key            = "medicojobs/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
